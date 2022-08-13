d465cfae9a94e2e1f51aef70939035552f889fa3
superluis1994 <alberto.sorto20@itca.edu.sv>
2022-08-13 09:18:17 -0600

    Practica 3

---
+++ P3/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int factorial(int);
+using namespace std;
+int main(int argc, char **argv){
+
+     cout <<"Programa que determina el numero combinatorio\n";
+
+    return 0;
+}
